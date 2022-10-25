a634df549e08728eff13054f701e37da822bdc66
Tejas Ratambe <99137410+TejasRatambe@users.noreply.github.com>
2022-10-25 20:55:17 +0530

    Add files via upload

---
+++ factorial.cpp.cpp
@@ -0,0 +1,17 @@
+//Find factorial of a no
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int fact = 1;
+	for(int i = 1; i<=10; i++)
+	{
+		fact = fact * i;
+		cout<< fact<<endl;
+	}
+	
+
+return 0;
+
+}
